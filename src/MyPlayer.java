333517624c2538a0e5caef54c9c36c72c706dc9d
28emilyma <28emilyma@gmail.com>
2024-05-18 23:37:39 -0400

    5/21 hw

--- src/MyPlayer.java
+++ src/MyPlayer.java
@@ -12,26 +12,82 @@ public class MyPlayer {
          * Add your code here.
          */
         for (int a=1;a<4;a++){
-            for (int b=0;b<4;b++){
-                for (int c=0;c<4;c++){
+            for (int b=0;b<4;b++) {
+                for (int c = 0; c < 4; c++) {
                     if (c <= b && b <= a) {
                         System.out.println(" ");
                         System.out.println(a + ": " + b + ": " + c);
                         System.out.println("resulting boards:");
+//
+//                        for (int newa = a; newa > 0; newa--){
+//                            for (int newb = b; newb >=0; newb--){
+//                                for (int newc = c; newc >=0; newc--){
+//                                    if(newc <= newb && newb <= newa){
+//                                        System.out.println(newa + ": " + newb + ": " + newc);
+//                                    }
+//                                }
+//                            }
+//                        }
 
-                        while(a>0) {
-                            if (a > b) {
-                                a = a - 1;
-                            } else if (a == b && b > c) {
-                                a = a - 1;
-                                b = b - 1;
-                            } else {
-                                a = a - 1;
-                                b = b - 1;
-                                c = c - 1;
+
+                        for (int newc = c; newc >= 0; newc--) {
+                            //System.out.println(a + ": " + b + ": " + newc + ": " + c);
+                            if (newc != c) {
+                                System.out.println(a + ": " + b + ": " + newc);
                             }
-                            System.out.println(a + ": " + b + ": " + c);
                         }
+                        for (int newb = b; newb >= 0; newb--) {
+                            if (c > newb) {
+                                int newc = newb;
+                                if (newb != b && newc != c) {
+                                    System.out.println(a + ": " + newb + ": " + newc);
+                                }
+                            } else {
+                                if (newb != b && c == c) {
+                                    System.out.println(a + ": " + newb + ": " + c);
+//                                }
+                                }
+                            }
+                        }
+                        for (int newa = a; newa > 0; newa--) {
+                            if (b > newa) {
+                                int newb = newa;
+                                int newc = newa;
+                                if (newa != a) {
+                                    System.out.println(newa + ": " + newb + ": " + newc);
+                                }
+                            } else {
+                                if (newa != a) {
+                                    System.out.println(newa + ": " + b + ": " + c);
+//                                }
+                                }
+//                            int newb = newa;
+//                            int newc = newa;
+//                            System.out.println(newa + ": " + newb + ": " + newc);
+                            }
+                        }
+//                        while (newc>=0){
+//                            newc =- newc;
+//                            System.out.println(a + ": " + b + ": " + newc);
+//                        }
+
+
+//                        int newa = a;
+//                        while(newa>0) {
+//                            int newb = b;
+//                            int newc = c;
+//                            if (newa > newb) {
+//                                newa = newa - 1;
+//                            } else if (newa == newb && newb > newc) {
+//                                newa = newa - 1;
+//                                newb = newb - 1;
+//                            } else {
+//                                newa = newa - 1;
+//                                newb = newb - 1;
+//                                newc = newc - 1;
+//                            }
+//                            System.out.println(newa + ": " + newb + ": " + newc);
+//                        }
 //                            for (int x = 1; x<a+1;x++){
 //                                for (int y = 0; y<b+1; y++){
 //                                    for (int z = 0; z < c+1; z++){
@@ -43,11 +99,11 @@ public class MyPlayer {
 //                            }
 
 
+                            }
                         }
                     }
                 }
             }
-        }
 
 
     public Point move(Chip[][] pBoard) {
